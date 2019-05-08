c2269c9e589d9835672d5e42466ff7301adf2ca4
jcoliver <jcoliver@email.arizona.edu>
2019-05-08 16:07:39 -0700

    Update parallel coordinate plot for gds data

--- gds-parallel-coordinate.R
+++ gds-parallel-coordinate.R
@@ -1,14 +1,28 @@
 # Parallel coordinate plot with preliminary GDS data
 # Jeff Oliver
 # jcoliver@email.arizona.edu
-# 2019-02-05
+# 2019-05-08
 
 rm(list = ls())
 
 ################################################################################
-gds <- read.csv(file = "data/gds.csv")
+gds <- read.csv(file = "data/gds-scores.csv")
 library(ggplot2)
 library(tidyr)
+library(dplyr)
+
+# Two columns are means of other columns
+gds$GDS.Data.representation.modeling <- rowMeans(gds[, c("GDS.Databases", "GDS.Math")], na.rm = TRUE)
+gds$GDS.Data.modeling <- rowMeans(gds[, c("GDS.Generative.Modeling", "GDS.Predictive.Modeling")], na.rm = TRUE)
+
+# Drop some columns that aren't needed
+drop.cols <- c("Row.Type", "GDS.Databases", "GDS.Math", 
+               "GDS.Generative.Modeling", "GDS.Predictive.Modeling", "X", 
+               "Site", "Date", "Notes")
+gds <- gds[, -which(colnames(gds) %in% drop.cols)]
+
+# Remove NA rows (i.e. UA B.S.)
+gds <- na.omit(gds)
 
 # Create column that is Institution X Program
 gds$inst.prog <- paste0(gds$Institution, gds$Program)
@@ -22,9 +36,28 @@ gds.long <- gds %>%
 # Drop the prefix "GDS."
 gds.long$area <- gsub(pattern = "GDS.", replacement = "", x = gds.long$area)
 
-#TODO: should probably order levels of area by score
-\
+# Change some names for easier plotting
+gds.long$area <- gsub(pattern = "Data.representation.modeling",
+                      replacement = "Data.representation",
+                      x = gds.long$area)
+gds.long$area <- gsub(pattern = "Data.",
+                      replacement = "Data\n",
+                      x = gds.long$area)
+
+# Relevel areas by scores
+area.means <- gds.long %>%
+  group_by(area) %>%
+  summarize(mean = mean(score, na.rm = TRUE))
+area.order <- area.means$area[order(area.means$mean, decreasing = TRUE)]
+gds.long$area <- factor(x = gds.long$area, levels = area.order)
+
 ggplot(data = gds.long, mapping = aes(x = area, y = score,
                                       group = inst.prog, color = Institution)) +
-  geom_line(position = position_dodge(width = 0.5)) +
-  geom_point(position = position_dodge(width = 0.5))
+  geom_line(position = position_dodge(width = 0.5), size = 0.5) +
+  geom_point(position = position_dodge(width = 0.5), size = 1.6) +
+  # Jitter for vertical axis won't work with two geoms because each has it's own
+  # random vertical offset
+  # geom_line(position = position_jitterdodge(dodge.width = 0.5, jitter.height = 0.1), size = 0.5) +
+  # geom_point(position = position_jitterdodge(dodge.width = 0.5, jitter.height = 0.1), size = 1.6) + 
+  theme_bw() +
+  theme(legend.position = "none")
